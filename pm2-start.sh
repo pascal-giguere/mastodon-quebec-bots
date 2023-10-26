87ca0e33013eea127abd2005acc973bda7a9ce79
Pascal Giguère <39274013+pascal-giguere@users.noreply.github.com>
2023-10-26 08:03:59 -0400

    update pm2 script

--- pm2-start.sh
+++ pm2-start.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-pm2 start yarn --interpreter bash --name mastodon-quebec-bots --log logs/pm2.log --time -- start
+pm2 start yarn --name mastodon-quebec-bots --log logs/pm2.log --time -- start
