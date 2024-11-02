bc347ea56cdfadd9f2fae75dd029626452255ca0
Pascal Giguère <39274013+pascal-giguere@users.noreply.github.com>
2024-11-02 11:56:20 -0400

    restore all mastofeed instances

--- src/mastofeed/index.ts
+++ src/mastofeed/index.ts
@@ -1,15 +1,12 @@
 import { Mastofeed } from 'mastofeed';
-/*
 import { laPresseFeed } from './laPresse';
 import { leDevoirFeed } from './leDevoir';
 import { radioCanadaFeed } from './radioCanada';
 import { lActualiteFeed } from './lActualite';
 import { pivotFeed } from './pivot';
-*/
 import { decrypteursFeed } from './decrypteurs';
 
-// const allFeeds: Mastofeed[] = [laPresseFeed, leDevoirFeed, radioCanadaFeed, lActualiteFeed, pivotFeed, decrypteursFeed];
-const allFeeds: Mastofeed[] = [decrypteursFeed];
+const allFeeds: Mastofeed[] = [laPresseFeed, leDevoirFeed, radioCanadaFeed, lActualiteFeed, pivotFeed, decrypteursFeed];
 
 export async function syncAllFeeds(): Promise<void> {
   console.log(`Syncing ${allFeeds.length} feeds...`);
